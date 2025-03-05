b8050d30dcf96cfed9cba698dc0a4e517fdc0ab0
VakoNarimanidze <119787310+VakoNarimanidze@users.noreply.github.com>
2025-03-05 08:18:11 +0400

    added resoponsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { CardsComponent } from './cards/cards.component';
 import { DetailsComponent } from './details/details.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
     {path: "",component: CardsComponent},
     {path:"cart",component:CartComponent},
-    {path:"details",component:DetailsComponent}
+    {path:"details",component:DetailsComponent},
+    {path:"**",component:ErrorComponent}
 ];
---
+++ src/app/error/error.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-error',
+  imports: [RouterModule],
+  templateUrl: './error.component.html',
+  styleUrl: './error.component.css'
+})
+export class ErrorComponent {
+
+}
